735799b0651df4c93601e01dc37fa606e36ee82a
ericawahe <ewahe@uiowa.com>
2026-03-31 12:34:20 -0500

    xml file edit

--- Project2_ws/xarm_nodes/setup.py
+++ Project2_ws/xarm_nodes/setup.py
@@ -11,7 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/project2_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/project2.launch.py']),
     ],
 
 
