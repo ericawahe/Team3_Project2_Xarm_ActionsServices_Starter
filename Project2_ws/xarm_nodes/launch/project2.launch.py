d3a6e28c5b4bc34f2f72ac5ab5e62633a44adaa7
ericawahe <ewahe@uiowa.com>
2026-04-01 13:52:58 -0500

    edit to launch file

--- Project2_ws/xarm_nodes/launch/project2.launch.py
+++ Project2_ws/xarm_nodes/launch/project2.launch.py
@@ -1,7 +1,15 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    usb_cam_params = PathJoinSubstitution([
+        FindPackageShare('xarm_nodes'),
+        'config',
+        'usb_cam_params.yaml'
+    ])
+     
     return LaunchDescription([
         Node(
             package='xarm_nodes',
@@ -21,7 +29,15 @@ def generate_launch_description():
 
         Node(
             package='xarm_nodes',
-            executable='camera_node',
+            executable='camera_node', 
+            output='screen'
+        ),
+
+        Node(
+            package='usb_cam',
+            executable='usb_cam_node_exe',
+            #parameters=['usb_cam_params.yaml'],
+            parameters=[usb_cam_params],
             output='screen'
         ),
 
